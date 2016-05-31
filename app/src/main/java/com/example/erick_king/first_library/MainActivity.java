c19b3400a437589a07dc52c261b4e8e54b2595ef
Erick-Munene <munenewahome@gmail.com>
2016-05-31 14:22:57 +0300

    added new library

---
+++ app/src/main/java/com/example/erick_king/first_library/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.erick_king.first_library;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.andexert.library.RippleView;
+
+import java.util.ArrayList;
+import com.andexert.library.RippleView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        rippleView.setOnRippleCompleteListener(new RippleView.OnRippleCompleteListener() {
+
+            @Override
+            public void onComplete(RippleView rippleView) {
+                Log.d("Sample", "Ripple completed");
+            }
+
+        });
+    }
+
+}
+
---
+++ app/src/main/java/com/example/erick_king/first_library/rippleView.java
@@ -0,0 +1,13 @@
+package com.example.erick_king.first_library;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class rippleView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ripple_view);
+    }
+}
