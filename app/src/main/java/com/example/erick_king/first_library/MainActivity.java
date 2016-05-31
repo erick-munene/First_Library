e50ed25659e0682773036585385e7b6c9fc89d9e
Erick-Munene <munenewahome@gmail.com>
2016-05-31 15:14:57 +0300

    added new library

--- app/src/main/java/com/example/erick_king/first_library/MainActivity.java
+++ app/src/main/java/com/example/erick_king/first_library/MainActivity.java
@@ -3,22 +3,8 @@ package com.example.erick_king.first_library;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListView;
-import android.widget.TextView;
 
-import com.andexert.library.RippleView;
-
-import java.util.ArrayList;
 import com.andexert.library.RippleView;
 
 public class MainActivity extends AppCompatActivity {
@@ -28,15 +14,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        rippleView.setOnRippleCompleteListener(new RippleView.OnRippleCompleteListener() {
-
+        final RippleView rippleView = (RippleView) findViewById(R.id.more);
+        rippleView.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onComplete(RippleView rippleView) {
-                Log.d("Sample", "Ripple completed");
+            public void onClick(View v) {
+                //TODO: onRippleViewClick
             }
-
         });
+
     }
 
 }
